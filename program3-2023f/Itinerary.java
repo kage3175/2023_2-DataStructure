55449be00d748beed1c09fb145b9ad48ceba39ae
hyunsoo <hjames2000@snu.ac.kr>
2023-11-17 16:02:03 +0900

    fds

--- program3-2023f/Itinerary.java
+++ program3-2023f/Itinerary.java
@@ -9,47 +9,12 @@ public class Itinerary
 
   // constructor
 
-  public class Time{
-    int hr;
-    int min;
-    int time;
-    int day;
-    int time_onlyhrmin;
-    public Time(String t, int d){
-      hr = Integer.parseInt(t) / 100;
-      min = Integer.parseInt(t) % 100;
-      day = d;
-      time = min + hr * 60 + d * 1440;
-      time_onlyhrmin = min + hr*60;
-    }
-    public int getDay(){
-      return day;
-    }
-    public int getTime(){
-      return time;
-    }
-    public int getTimeHrMin(){
-      return time_onlyhrmin;
-    }
-    public void add(String t){
-      int hrInc = Integer.parseInt(t) / 100;
-      int minInc = Integer.parseInt(t) % 100;
-      min += minInc;
-      if(min >= 60){
-        min-=60;
-        hrInc++;
-      }
-      hr += hrInc;
-      if(hr >= 24){
-        hr-=24;
-        day+=1;
-      }
-      time = min + hr * 60 + day * 1440;
-    }
-  }
-
   public Itinerary(LinkedList<Flight> ticket) {
     planned = ticket;
+  }
+
+  public Itinerary() {
+    planned = null;
   }
 
   public boolean isFound() { //False if plan is  empty(null)
